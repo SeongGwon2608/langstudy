c960f93c651c416d516246ccb186846fec030453
최성권 <insa8029@naver.com>
2023-08-08 22:20:28 +0900

    [MainActivity] 화면에 결과값 띄워주도록 화면 적용

--- LanguageStudy/app/src/main/java/com/sgchoi/languagestudy/MainActivity.kt
+++ LanguageStudy/app/src/main/java/com/sgchoi/languagestudy/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.sgchoi.languagestudy.level1.*
 import com.sgchoi.languagestudy.ui.theme.LanguageStudyTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    PrintResult(result = littleString("3141592", "271").toString())
                 }
             }
         }
@@ -30,9 +31,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun PrintResult(result: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = result,
         modifier = modifier
     )
 }
@@ -41,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     LanguageStudyTheme {
-        Greeting("Android")
+        PrintResult("Android")
     }
 }
